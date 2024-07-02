0fc5bc245d016251d23c7b84db2a78634a8cdade
bkhtiar <bakhtiarimam801@gamil.com>
2024-07-02 21:10:40 +0700

    menambah 1 skil set baru, menambah button input

--- main.js
+++ main.js
@@ -1,49 +1,61 @@
-const hamburgerMenu = document.querySelector('nav .hamburgerMenu');
-const menu = document.querySelector('nav .navMenu');
-const button = document.querySelector('.hero button');
-const imgProfile = document.querySelector('.hero .containerImg img');
-const containerProfileImg = document.querySelector('.hero .containerImg')
+const hamburgerMenu = document.querySelector("nav .hamburgerMenu");
+const menu = document.querySelector("nav .navMenu");
+const button = document.querySelector(".hero button");
+const imgProfile = document.querySelector(".hero .containerImg img");
+const containerProfileImg = document.querySelector(".hero .containerImg");
+const inputs = document.querySelectorAll("input, textarea");
 const imgProfilePath = [
-  'img/profilePictures/picture1.jpg',
-  'img/profilePictures/picture2.jpg',
-  'img/profilePictures/picture3.jpg',
-  'img/profilePictures/picture4.jpg',
-  'img/profilePictures/picture5.jpg',
-  'img/profilePictures/picture6.jpg'];
+  "img/profilePictures/picture1.jpg",
+  "img/profilePictures/picture2.jpg",
+  "img/profilePictures/picture3.jpg",
+  "img/profilePictures/picture4.jpg",
+  "img/profilePictures/picture5.jpg",
+  "img/profilePictures/picture6.jpg",
+];
 let i = 0;
 
-document.addEventListener('DOMContentLoaded', (ev) => {
-  document.querySelectorAll('.navMenu a[href]')
-    .forEach(el => {
-      el.addEventListener('click', function (e) {
-        e.preventDefault()
-        const hash = this.getAttribute('href')
-        location.replace(hash)
-      })
-    })
-})
-
-hamburgerMenu.addEventListener('click', function (e) {
-  menu.classList.toggle('active')
-  hamburgerMenu.classList.toggle('active')
-  e.preventDefault()
+document.addEventListener("DOMContentLoaded", (ev) => {
+  document.querySelectorAll(".navMenu a[href]").forEach((el) => {
+    el.addEventListener("click", function (e) {
+      e.preventDefault();
+      const hash = this.getAttribute("href");
+      location.replace(hash);
+    });
+  });
 });
 
+hamburgerMenu.addEventListener("click", function (e) {
+  menu.classList.toggle("active");
+  hamburgerMenu.classList.toggle("active");
+  e.preventDefault();
+});
 
-document.addEventListener('click', function (e) {
+document.addEventListener("click", function (e) {
   if (!hamburgerMenu.contains(e.target) && !menu.contains(e.target)) {
-    menu.classList.remove('active')
-    hamburgerMenu.classList.remove('active')
+    menu.classList.remove("active");
+    hamburgerMenu.classList.remove("active");
   }
-})
+});
 
 setInterval(() => {
-  i++
+  i++;
   if (i === imgProfilePath.length) {
-    i = 0
+    i = 0;
   }
-  const newImg = document.createElement('img');
-  newImg.src = imgProfilePath[i]
-  newImg.className = "fadeOut"
-  containerProfileImg.appendChild(newImg)
-}, 5000)
+  const newImg = document.createElement("img");
+  newImg.src = imgProfilePath[i];
+  newImg.className = "fadeOut";
+  containerProfileImg.appendChild(newImg);
+}, 5000);
+
+inputs.forEach((input, index) => {
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault(); // Mencegah form submit
+      const nextInput = inputs[index + 1];
+      if (nextInput) {
+        nextInput.focus();
+      }
+    }
+  });
+});

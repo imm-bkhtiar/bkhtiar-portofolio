e94d0a6a726e10bef5b5fc9ea178907687a759e7
bkhtiar <bakhtiarimam801@gamil.com>
2024-08-09 17:27:37 +0700

    menambahkan fitu auto hidden navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,37 @@
 const hamburgerBtn = document.querySelector(".navContainer .hamburgerMenu");
+const navMenu = document.querySelector(".navContainer .navMenu");
 
 hamburgerBtn.addEventListener("click", () => {
-  const items = document.querySelectorAll(".navContainer .hamburgerMenu a");
-  const navMenu = document.querySelector(".navContainer .navMenu");
   navMenu.classList.toggle("active");
   hamburgerBtn.style.animation = "active 0.5s ease";
+
   setTimeout(() => {
     hamburgerBtn.style.animation = "none";
   }, 500);
+
+  if (!navMenu.classList.contains("active")) {
+    hidden();
+  }
 });
+
+document.addEventListener("click", (e) => {
+  if (!navMenu.contains(e.target) && !hamburgerBtn.contains(e.target)) {
+    navMenu.classList.remove("active");
+    hidden();
+  }
+});
+
+document.addEventListener("scrollend", () => {
+  navMenu.parentElement.style.opacity = 1;
+  navMenu.parentElement.style.transform = "translateY(0)";
+});
+
+const hidden = () => {
+  setTimeout(() => {
+    if (!navMenu.classList.contains("active")) {
+      console.log("berhasil");
+      navMenu.parentElement.style.opacity = 0;
+      navMenu.parentElement.style.transform = "translateY(100%)";
+    }
+  }, 5000);
+};

1005bbcf4ea1e0b46f3090189ea4a8d5511d0e64
imm-bkhtiar <bakhtiarimam801@gmail.com>
2025-02-12 15:16:56 +0700

    membuat library slider sendiri untuk bagian project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+import { slider } from "../../lib/lib.js";
 const hamburger = document.querySelector(".hamburger input");
 const nav = document.querySelector("nav");
 let triggerScroll = window.screenY;
@@ -20,39 +21,9 @@ document.addEventListener("scroll", () => {
 
 // ------ Project ------
 
-const reload = () => {
-  return {
-    container: document.querySelector("#project main .container"),
-    items: document.querySelectorAll("#project main .container a"),
-  };
-};
-const time = 5000;
-
-reload().container.scrollLeft = reload().items[0].clientWidth;
-reload().container.prepend(reload().items[reload().items.length - 1]);
-
-let userAgent = navigator.userAgent.toLowerCase();
-
-const autoSlide = () => {
-  const container = reload().container;
-  const items = reload().items;
-  container.scrollLeft += items[0].clientWidth;
-  container.addEventListener("scrollend", () => {
-    container.appendChild(items[0]);
-    if (
-      userAgent.includes("chrome") &&
-      !userAgent.includes("edg") &&
-      !userAgent.includes("opr")
-    ) {
-      container.scroll({
-        left: items[1].clientWidth,
-        behavior: "instant"
-      })
-    }
-  });
-  setTimeout(() => {
-    requestAnimationFrame(autoSlide);
-  }, time);
-};
-
-requestAnimationFrame(autoSlide);
+slider([
+  './assets/img/project/1.png',
+  './assets/img/project/2.png',
+  './assets/img/project/3.png',
+  './assets/img/project/4.png'
+])
